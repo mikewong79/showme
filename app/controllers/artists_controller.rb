7271721a5e6812dd6fe0f7c56357401bf30a1fbe
shanefitzgerald <shanefitzgerald@Shannons-MacBook-Air.local>
2014-05-28 18:47:09 -0700

    artist test complete

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,39 @@
+class ArtistsController < ApplicationController
+ before_action :set_artist, only: [:show, :edit, :update, :destroy]
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+  	
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def edit
+  	
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artists_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :genre)
+  end
+end
+
+
