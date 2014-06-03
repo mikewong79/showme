00c47f64f1c6a36da3202f45c51d31954ca2e153
Mike Wong <mikewong79@gmail.com>
2014-06-02 17:11:05 -0700

    removed nested routes

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,36 +1,36 @@
 class OwnersController < ApplicationController
 # before_action :set_owner, only:[:show, :edit, :update]
-before_action :authenticate_owner!, only:[:edit, :update, :destroy]
+# before_action :authenticate_owner!, only:[:edit, :update, :destroy]
 
-  def index
-    @owners = Owner.all
-  end
+#   def index
+#     @owners = Owner.all
+#   end
 
-  def show
-  end
+#   def show
+#   end
 
-  def new
-    @owner = Owner.new
-  end
+#   def new
+#     @owner = Owner.new
+#   end
 
-  def edit
-  end
+#   def edit
+#   end
 
-  def create
-    @owner=Owner.new(owner_params)
-    if @owner.save
-      redirect_to owners_path
-    else
-      render :new
-    end
-  end
-  protected
+#   def create
+#     @owner=Owner.new(owner_params)
+#     if @owner.save
+#       redirect_to owners_path
+#     else
+#       render :new
+#     end
+#   end
+#   protected
 
-  def set_owner
-    @owner = Owner.find(params[:id])
-  end
+#   def set_owner
+#     @owner = Owner.find(params[:id])
+#   end
 
-  def owner_params
-    params.require(:owner).permit(:first_name, :last_name, :email)
-  end
+#   def owner_params
+#     params.require(:owner).permit(:first_name, :last_name, :email)
+#   end
 end
