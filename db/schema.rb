2fa1ab7ca50424a45c687990b1e85c30aa857a90
Mike Wong <mikewong79@gmail.com>
2014-06-05 11:04:43 -0700

    dropped performance reference from artist

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603003102) do
+ActiveRecord::Schema.define(version: 20140605175727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,9 @@ ActiveRecord::Schema.define(version: 20140603003102) do
   create_table "artists", force: true do |t|
     t.string   "name"
     t.string   "genre"
-    t.integer  "performance_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "artists", ["performance_id"], name: "index_artists_on_performance_id", using: :btree
 
   create_table "artists_performances", id: false, force: true do |t|
     t.integer "performance_id", null: false
